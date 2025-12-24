5183e9cc3d02a45b726ee57f81ef8aa0291e9025
Saeid Behjat <orcastack@gmail.com>
2025-12-24 13:52:39 +0330

    showing coins in table

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import chartUp from "../../assets/chart-up.svg"
+import chartDown from "../../assets/chart-down.svg"
 
 function TableCoin({ coins }) {
   console.log(coins);
   return (
     <>
-      <p>table coin</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <img src={coin.image} alt={coin.name} />
+                <span>{coin.symbol.toUpperCase()}</span>
+              </td>
+              <td>{coin.name}</td>
+              <td>${coin.current_price.toLocaleString()}</td>
+              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+              <td>{coin.total_volume.toLocaleString()}</td>
+              <td><img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} alt="" /></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
